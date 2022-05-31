cbfa21b8e97a59ddcc4b8bf2c6192ee3f14a147e
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-05-31 10:23:05 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,17 @@ const gaintype = querymanager.has("gaintype") ? querymanager.get("gaintype") : "
 var number = new ExpantaNum("2")
 function lngi() {
 	if (gaintype == "add") {
-		number = number.add(new ExpantaNum(gain))
+		number = number.add(new ExpantaNum(gain).div(30))
 	}
-	document.getElementById("number").innerHTML = number
+	if (gaintype == "mul") {
+		number = number.mul(new ExpantaNum(gain).div(30))
+	}
+	if (gaintype == "pow") {
+		number = number.pow(new ExpantaNum(gain).div(30))
+	}
+	if (gaintype == "tet") {
+		number = number.tetrate(new ExpantaNum(gain).div(30))
+	}
+	document.getElementById("number").innerHTML = number.toStringWithDecimalPlaces(4)
 }
 setInterval(lngi,1000/30)
