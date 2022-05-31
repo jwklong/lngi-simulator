3b89ea449fe69c85f2a080ec42fc0d5dc28ee95d
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-05-31 10:12:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const querymanager = new URLSearchParams(window.location.search)
-if (querymanager.has("test")) {
-	console.log(querymanager.get("test"))
+const gain = querymanager.has("gain") ? querymanager.get("gain") : "1"
+const gaintype = querymanager.has("gaintype") ? querymanager.get("gaintype") : "add"
+
+var number = new ExpantaNum("2")
+function lngi() {
+	if (gaintype == "add") {
+		number = number.add(new ExpantaNum(gain))
+	}
+	document.getElementById("number").innerHTML = number
 }
+setInterval(lngi,1000/30)
